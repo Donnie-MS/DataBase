aeb972d68ff6dcbb65d401f845064bf6f91863e0
Orejas 2016 <joaomasgosandoval@gmail.com>
2025-10-22 20:01:40 -0300

    idk

--- ClassOne.sql
+++ ClassOne.sql
@@ -1,1 +1,18 @@
-create database Alumnos;
+CREATE DATABASE IF NOT EXISTS Students;
+
+DROP DATABASE IF EXISTS Students;
+
+USE Students;
+
+CREATE TABLE Award (
+	awardId INT, 
+    award CHAR(40));
+
+CREATE TABLE PersonInCharge (
+    personInChargeId INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100),
+    DNI INT,
+    EDAD INT DEFAULT 18
+);
+
+SELECT * FROM Award;
