65cf9f6ab235fa50c021bd5a83bc491d21e15960
Orejas 2016 <joaomasgosandoval@gmail.com>
2025-11-03 12:44:03 -0300

    s

--- multiTableOne.sql
+++ multiTableOne.sql
@@ -51,3 +51,57 @@ CREATE TABLE ProvidedBy (
     supplierID_FK INT NOT NULL,
     FOREIGN KEY (supplierID_FK) REFERENCES Supplier(supplierID)
 );
+
+DROP TABLE Warehouse;
+DROP TABLE ProvidedBy;
+DROP TABLE ComposedOf;
+DROP TABLE Contain;
+DROP TABLE Supplier;
+DROP TABLE Material;
+DROP TABLE City;
+DROP TABLE Article;
+
+/*INSERTAR DATOS*/
+SELECT * FROM Warehouse;
+SELECT * FROM Article;
+SELECT * FROM Contain;
+
+INSERT INTO Warehouse (personInCharge)
+VALUES ('Alfredo'), ('Esteban'), ('Juan'), ('Roberto');
+
+INSERT INTO Article (`description`, price) 
+VALUES  ("Bread", 130.70), ("Pastries", 300.00), ("Chesse cake", 450.87), ("Pasta Frola", 278.90);
+
+INSERT INTO Material (`description`)
+VALUES ("Oil"), ("Flour"), ("Yeast"), ("egg"), ("sugar"), ("salt"), ("water");
+
+INSERT INTO City(`name`)
+VALUES ("La plata"), ("Capital FEDERAL"), ("Ramos Mejia"), ("La matanza");
+
+INSERT INTO Supplier(`name`, address, cityID_FK) 
+VALUES ("Arcor", "Ayacucho 1234", 1),
+ ("Molinos", "Yatay 456", 4),
+ ("Ledesma", "Mario Bravo 987", 1),
+ ("Marolio", "Potosi 098", 2),
+ ("Glaciar", "Sarmiento 555", 3);
+
+INSERT INTO Contain(number_FK, articleID_FK) 
+VALUES (1, 1), (1, 2), (1,3), (1,4),
+(2, 3), (2, 4),
+(3, 1), (4, 2);
+
+INSERT INTO ComposedOf(articleID_FK, materialID_FK)
+VALUES (1, 2), (1, 3),
+(2, 2), (2, 5), (2, 7), 
+(3, 1), (3, 2), (3, 6),
+(4, 7);
+
+INSERT INTO ProvidedBy(materialID_FK, supplierID_FK)
+VALUES (2, 3), (3, 5), (4, 4), (5, 2), (6, 2), (7,5);
+
+SELECT s.`name` AS Supplier, c.`name` AS City FROM Supplier s
+JOIN City c ON c.cityID = s.cityID_FK;
+/*
+FROM City c
+JOIN Supplier s ON s.cityID_FK = c.cityID;
+*/
