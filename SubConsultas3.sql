41afaed36b4cefdae5f2ed428ab6d7ac35fa234f
Orejas 2016 <joaomasgosandoval@gmail.com>
2025-11-14 15:18:12 -0300

    a

--- SubConsultas3.sql
+++ SubConsultas3.sql
@@ -1,3 +1,4 @@
+USE subConsultas3;
 CREATE TABLE Warehouse(
 	`Number` INT PRIMARY KEY AUTO_INCREMENT,
     `Name` VARCHAR(20) NOT NULL,
@@ -14,6 +15,10 @@ CREATE TABLE Material(
 	MaterialID INT PRIMARY KEY AUTO_INCREMENT,
     `Description` VARCHAR(20) NOT NULL
 );
+CREATE TABLE City (
+	CityID INT PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(20) NOT NULL
+);
 
 CREATE TABLE Supplier(
 	SupplierID INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,10 +28,6 @@ CREATE TABLE Supplier(
     FOREIGN KEY (CityID_FK) REFERENCES City(CityID)
 );
 
-CREATE TABLE City (
-	CityID INT PRIMARY KEY AUTO_INCREMENT,
-    `Name` VARCHAR(20) NOT NULL
-);
 
 CREATE TABLE Contains (
 	ContainsID INT PRIMARY KEY AUTO_INCREMENT,
@@ -89,3 +90,109 @@ VALUES ('Arcor', 'Ayacucho 1234', 1),
 ('Glaciar', 'Sarmiento 555', 3),
 ('Johnson', 'Potosi 123', 1);
 
+INSERT INTO Contains(Number_FK, ArticleID_FK)
+VALUES (001, 001),
+(001, 002),
+(001, 003),
+(001, 004),
+(002, 003),
+(002, 004),
+(003, 001),
+(004, 002);
+
+INSERT INTO ComposedOf (ArticleID_FK, MaterialID_FK)
+VALUES (001, 001),
+(001, 002),
+(001, 003),
+(002, 002),
+(002, 005),
+(002, 007),
+(003, 001),
+(003, 002),
+(003, 006),
+(004, 007);
+
+INSERT INTO ProvidedBy(MaterialID_FK, SupplierID_FK)
+VALUES (001, 1),
+(002, 3),
+(003, 5),
+(004, 4),
+(005, 2),
+(006, 2),
+(007, 5),
+(005, 5);
+
+SELECT * FROM Supplier;
+SELECT S.Name FROM Supplier AS S
+WHERE NOT EXISTS (SELECT 1 
+FROM ProvidedBy AS P
+WHERE P.SupplierID_FK = S.SupplierID
+);
+/* No funcioan si hay un null
+SELECT Name FROM Supplier 
+WHERE SupplierID NOT IN (
+	SELECT SupplierID_FK FROM ProvidedBy
+);
+*/
+/*
+SELECT S.Name FROM Supplier AS S
+LEFT JOIN ProvidedBy AS P ON S.SupplierID = P.SupplierID_FK
+WHERE P.SupplierID_FK IS NULL;
+*/
+/*
+#2) Listar los códigos y descripción de los materiales que provea
+el proveedor 2 y no los provea el proveedor 5
+*/
+
+SELECT M.MaterialID, M.Description 
+FROM Material M
+WHERE EXISTS
+(SELECT 1
+FROM ProvidedBy P
+WHERE P.SupplierID_FK = 2 AND P.MaterialID_FK = M.MaterialID
+) AND NOT EXISTS (
+	SELECT 1 FROM ProvidedBy P
+    WHERE P.SupplierID_FK = 5 AND P.MaterialID_FK = M.MaterialID
+);
+/*
+#3) Listar número y nombre de almacenes que contienen los
+artículos de descripción ‘Pan’ y los de descripción ‘Facturas’
+(ambos).
+*/
+
+SELECT W.Number, W.Name FROM Warehouse W
+WHERE W.Number IN (
+	SELECT Con.Number_FK FROM Article A JOIN Contains Con ON Con.ArticleID_FK = A.ArticleID
+    WHERE A.description LIKE "%Bread%"
+) AND W.Number IN (
+	SELECT Con.Number_FK FROM Article A JOIN Contains Con ON Con.ArticleID_FK = A.ArticleID
+    WHERE A.description Like "%Pastries%"
+);
+   
+/*#4) Listar la descripción de artículos compuestos por todos los
+materiales*/
+
+SELECT A.ArticleID, A.description 
+FROM ComposedOf Com JOIN Article A 
+ON Com.ArticleID_Fk = A.ArticleID
+GROUP BY A.ArticleID, A.Description
+HAVING COUNT(*) = (SELECT COUNT(*) FROM MATERIAL);
+
+/*
+#5) Hallar los códigos y nombres de los proveedores que proveen
+al menos un material que se usa en algún artículo cuyo precio es
+mayor a $300
+*/
+
+SELECT DISTINCT S.SupplierID, S.Name FROM ProvidedBy Pro 
+JOIN Supplier S ON Pro.SupplierID_FK = S.SupplierID
+JOIN ComposedOf C ON C.MaterialID_FK = Pro.MaterialID_FK
+JOIN Article A ON C.ArticleID_Fk = A.ArticleID
+WHERE A.Price >= 300;
+
+/*
+#6) Listar la descripción de los artículos de mayor precio
+*/
+
+SELECT A.description FROM Article A
+WHERE A.Price = (SELECT MAX(A.PRICE) FROM Article A)
